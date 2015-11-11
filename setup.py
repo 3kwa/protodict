0668383e2c5e2805a0be1e7574ad8ff2bef53f27
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2015-11-11 18:02:48 +1100

    setup_requires suck?

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='protodict',
     description='a small Python library for 2 ways conversion between dicts '
         'and protocol buffers.',
-    version='1.0.0',
+    version='1.0.1',
     author='Eugene Van den Bulke',
     author_email='eugene.vandenbulke@gmail.com',
     url='https://github.com/3kwa/protodict',
@@ -14,8 +14,8 @@ setup(
     install_requires=['protobuf>=2.3.0'],
     package_dir={'':'src'},
     py_modules=['protodict'],
-    setup_requires=['protobuf>=2.3.0', 'nose>=1.0', 'coverage', 'nosexcover'],
-    test_suite = 'nose.collector',
+    #setup_requires=['protobuf>=2.3.0', 'nose>=1.0', 'coverage', 'nosexcover'],
+    #test_suite = 'nose.collector',
     classifiers=[
         'Programming Language :: Python',
         'Development Status :: 5 - Production/Stable',
